f891594ca863718507e5d76096ed934b65b506c7
Ammypendent <jualco.hammerwing@gmail.com>
2014-01-25 22:20:07 -0800

    Finished Lvl 1 layout and started lvl 2

--- Assets/Scripts/Level/Visibility.cs
+++ Assets/Scripts/Level/Visibility.cs
@@ -4,34 +4,26 @@ using System.Collections;
 public class Visibility : MonoBehaviour {
 
 	public int whichPlayerCanSeeMe;
-	private GameObject localPlayer;
-	private Player playerStats;
-
 
 	void Start () 
 	{
-		localPlayer = GameObject.Find("PlayerOne");
-		playerStats = localPlayer.GetComponent<Player>();
-
-		SetVisibility();
-
+		if (GlobalPlayer.playerID != null)
+			SetVisibility();
+		else
+			print ("Global Player ID is Null! Everything stays visible!");
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
 	public void SetVisibility()
 	{
 		switch (whichPlayerCanSeeMe)
 		{
 		case 0:
-			if (playerStats.playerID != 0)
+			if (GlobalPlayer.playerID != 0)
 				renderer.enabled = false;
 			break;
 		case 1:
-			if (playerStats.playerID != 1)
+			if (GlobalPlayer.playerID != 1)
 				renderer.enabled = false;
 			break;
 		default:
